c27bc260fb21ab4216e8a66f7f5c0b4d4c12ecf4
hahahahallo <96128700+hahahahallo@users.noreply.github.com>
2022-01-11 15:19:16 +0100

    Add files via upload

--- Auftrag Prüfungssoftware 23.11.21.py
+++ Auftrag Prüfungssoftware 23.11.21.py
@@ -1,9 +1,9 @@
-for i in range(0, 3):
-    antwort = input("Was ist die Hauptstadt der Schweiz? ")
-    if antwort == "Bern":
-        print("Gratulation, sie haben die Prüfung bestanden!")
-        break
-    elif i == 2:
-        print("Sie haben die Prüfung leider nicht bestanden.")
+for i in range(0, 3): #wie viele Versuche man haben kann
+    antwort = input("Was ist die Hauptstadt der Schweiz? ") #Gibt eine Stadt ein
+    if antwort == "Bern": #wenn sie Bern 
+        print("Gratulation, sie haben die Prüfung bestanden!") #gibt sie aus das sie die Prüfung bestanden haben
+        break #Loops wird abgebrochen
+    elif i == 2: # sonst
+        print("Sie haben die Prüfung leider nicht bestanden.") #gibt aus das sie die Prüfung nicht bestanden haben
     else:
-        print("Leider Falsch, sie haben noch " + str(2 - i) + " Versuche")
+        print("Leider Falsch, sie haben noch " + str(2 - i) + " Versuche") #gibt aus wie viele Versuche sie noch haben
